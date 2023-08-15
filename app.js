ee4ddb7de2a013dcdf33d6f867e8825f53db9bc8
Tyler Marcott <tylerjmarcott@gmail.com>
2023-08-15 16:15:30 -0600

    Working on sec5

--- app.js
+++ app.js
@@ -83,20 +83,60 @@ const people = [{
 
 function drawPeople() {
   locations.forEach((location) => {
-    console.log(location)
+    // console.log(location)
     let inArea = people.filter((people) => people.location == location)
     let peopleEmojis = inArea.map((people) => people.picture + people.name)
-    console.log(peopleEmojis)
+    // console.log(peopleEmojis)
     document.getElementById(location).innerText = peopleEmojis.join(' ')
-
-
-
   })
 }
 
-function attack() {
+//NOTE: foreach shortcut to make a foreach layout
 
+function peopleRun() {
+  people.forEach((person) => {
+    if (person.emoji != '🦇') {
+
+      let randomIndex = Math.floor(Math.random() * locations.length)
+      console.log(randomIndex)
+      let newArea = locations[randomIndex]
+      person.location = newArea
+
+
+    }
+  });
 }
+
+
+
+
+function attack(emoji) {
+  console.log(emoji)
+
+  let bat = '🦇'
+
+  // when a button is clicked, get all the people who have the same location as the button
+
+  let inArea = people.filter((people) => people.location == emoji)
+
+  let peoplePictures = inArea.map((people) => people.picture = bat)
+
+  // console.log(peoplePictures)
+
+  drawPeople()
+  peopleRun()
+  checkScore()
+}
+
+function checkScore() {
+  let bat = '🦇'
+
+  let turned = people.filter((people) => people.picture == bat)
+  if (turned.length == people.length) {
+    window.alert("the vampires have taken over!!!!!!! 🧛🏻‍♀️🧛🏿🧛🏽‍♂️🦇")
+  }
+}
+
 
 
 drawPeople()
