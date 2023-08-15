516cf421b1d60f551853dfb6cb98c2a3df180811
Tyler Marcott <tylerjmarcott@gmail.com>
2023-08-15 13:58:28 -0600

    Init commit. Layout created.

---
+++ app.js
@@ -0,0 +1,65 @@
+const locations = [
+  '🏤', '🏥', '🏭', '🏢', '🏣'
+]
+
+const people = [{
+  name: 'Jimbo',
+  picture: '🤵',
+  location: '🏤'
+},
+{
+  name: 'Sammy',
+  picture: '🙆‍♀️',
+  location: '🏤'
+},
+{
+  name: 'Michael',
+  picture: '👷',
+  location: '🏤'
+},
+{
+  name: 'Robert',
+  picture: '👷',
+  location: '🏥'
+},
+{
+  name: 'Terry',
+  picture: '🤴',
+  location: '🏥',
+},
+{
+  name: 'Bill',
+  picture: '🕵️',
+  location: '🏥',
+},
+{
+  name: 'Marie',
+  picture: '👩‍🍳',
+  location: '🏭',
+},
+{
+  name: 'Mykeal',
+  picture: '💂',
+  location: '🏭',
+},
+{
+  name: 'Phil',
+  picture: '🧜‍♂️',
+  location: '🏭',
+},
+{
+  name: 'Wilson',
+  picture: '🏐',
+  location: '🏢',
+},
+{
+  name: 'Wendy',
+  picture: '👩‍⚕️',
+  location: '🏢',
+},
+{
+  name: 'Jeremy',
+  picture: '🦹',
+  location: '🏢',
+}
+]
